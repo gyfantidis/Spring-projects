f18ea214cbf3685d7b7b74d45ffc1a70ddd56203
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-01-16 17:11:04 +0200

    First Commit

---
+++ EAP-PLI/src/main/java/net/yfantidis/EAPPLI/EapPliApplication.java
@@ -0,0 +1,13 @@
+package net.yfantidis.EAPPLI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EapPliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EapPliApplication.class, args);
+	}
+
+}
---
+++ EAP-PLI/src/test/java/net/yfantidis/EAPPLI/EapPliApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yfantidis.EAPPLI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EapPliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
